b6015ba3a8daafb51302850b5891cdb2d026518d
Kenji KATO <katoken@katoken-0215.sakura.ne.jp>
2013-08-04 12:25:44 +0900

    Install haskell-mode

---
+++ conf/init-haskell-mode.el
@@ -0,0 +1,7 @@
+(load "~/.emacs.d/elisp/haskell-mode-13.7/haskell-mode.el")
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
