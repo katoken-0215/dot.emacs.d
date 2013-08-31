d4197e7bc03b8d8dec17bcded7b78c0e7ebe49ec
Kenji KATO <katoken@katoken-0215.sakura.ne.jp>
2013-09-01 02:02:50 +0900

    Update init-haskell-mode.el; Indentation problem

--- conf/init-haskell-mode.el
+++ conf/init-haskell-mode.el
@@ -1,8 +1,6 @@
 (add-to-list 'load-path "~/.emacs.d/elisp/haskell-mode-13.7")
-(load-library "haskell-mode-autoloads")
+(load-library "haskell-mode")
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
