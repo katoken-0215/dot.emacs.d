a6009335a5bd3fb4556256ae047cb41cfae830cd
Kenji KATO <kenji.kato@access-company.com>
2013-09-18 12:29:10 +0900

    Assign C-c a to anything

--- conf/init-anything.el
+++ conf/init-anything.el
@@ -3,8 +3,9 @@
 (require 'anything-c-moccur)
 (global-set-key (kbd "M-o") 'anything-c-moccur-occur-by-moccur)
 (global-set-key (kbd "C-M-o") 'anything-c-moccur-dmoccur)
+(global-set-key (kbd "C-c a") 'anything)
 (add-hook 'dired-mode-hook
-	  '(lambda ()
-	     (local-set-key (kbd "O") 'anything-c-moccur-dired-do-moccur-by-moccur)))
+          '(lambda ()
+             (local-set-key (kbd "O") 'anything-c-moccur-dired-do-moccur-by-moccur)))
 
 (global-set-key (kbd "M-y") 'anything-show-kill-ring)
