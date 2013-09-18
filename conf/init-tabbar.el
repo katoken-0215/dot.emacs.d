655fa872af05d41b10556757f9bdf220d5c89d8f
Kenji KATO <katoken@katoken-0215.sakura.ne.jp>
2013-09-18 10:00:23 +0900

    Update init-tabbar.el

--- conf/init-tabbar.el
+++ conf/init-tabbar.el
@@ -1,60 +1,30 @@
-;; @ tabbar
 (require 'cl)
 (require 'tabbar)
-
-;; scratch buffer 以外をまとめてタブに表示する
-(setq tabbar-buffer-groups-function
-      (lambda (b) (list "All Buffers")))
-(setq tabbar-buffer-list-function
-      (lambda ()
-	(remove-if
-	 (lambda(buffer)
-	   (unless (string-match (buffer-name buffer) "\\(*scratch*\\|*Apropos*\\|*shell*\\|*eshell*\\|*Customize\\)")
-	     (find (aref (buffer-name buffer) 0) " *"))
-	   )
-	 (buffer-list))))
-
-;; Color Settings
-(set-face-attribute
-  'tabbar-default-face nil
-  :background "gray90") ;バー自体の色
-(set-face-attribute
- 'tabbar-separator-face nil
- :height 0.7)
-
-(set-face-attribute ;非アクティブなタブ
- 'tabbar-unselected-face nil
- :background "gray90"
- :foreground "black"
- :box nil)
-(set-face-attribute ;アクティブなタブ
- 'tabbar-selected-face nil
- :background "black"
- :foreground "white"
- :box nil)
-
-
-;; tabbarを有効にする
 (tabbar-mode 1)
 
-;; ボタンをシンプルにする
-(setq tabbar-home-button-enabled "")
-(setq tabbar-scroll-right-button-enabled "")
-(setq tabbar-scroll-left-button-enabled "")
-(setq tabbar-scroll-right-button-disabled "")
-(setq tabbar-scroll-left-button-disabled "")
+(tabbar-mwheel-mode nil)
 
-;; Ctrl-Tab, Ctrl-Shift-Tab でタブを切り替える
-(dolist (func '(tabbar-mode tabbar-forward-tab tabbar-forward-group tabbar-backward-tab tabbar-backward-group))
-  (autoload func "tabbar" "Tabs at the top of buffers and easy control-tab navigation"))
-(defmacro defun-prefix-alt (name on-no-prefix on-prefix &optional do-always)
-  `(defun ,name (arg)
-     (interactive "P")
-     ,do-always
-     (if (equal nil arg)
-	 ,on-no-prefix
-       ,on-prefix)))
-(defun-prefix-alt shk-tabbar-next (tabbar-forward-tab) (tabbar-forward-group) (tabbar-mode 1))
-(defun-prefix-alt shk-tabbar-prev (tabbar-backward-tab) (tabbar-backward-group) (tabbar-mode 1))
-(global-set-key [(control tab)] 'shk-tabbar-next)
-(global-set-key [(control shift tab)] 'shk-tabbar-prev)
+(setq tabbar-buffer-groups-function nil)
+
+(dolist (btn '(tabbar-buffer-home-button
+               tabbar-scroll-left-button
+               tabbar-scroll-right-button))
+  (set btn (cons (cons "" nil)
+                 (cons "" nil))))
+
+(set-face-attribute ; color of tabbar
+  'tabbar-default nil
+   :background "white"
+   :family "Inconsolata"
+   :height 1.0)
+(set-face-attribute ; active tab
+  'tabbar-selected nil
+   :background "black"
+   :foreground "white"
+   :weight 'bold
+   :box nil)
+(set-face-attribute ; inactive tab
+  'tabbar-unselected nil
+   :background "white"
+   :foreground "black"
+   :box nil)
