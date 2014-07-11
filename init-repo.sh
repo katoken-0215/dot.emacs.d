a60d097cfa27ce959861514eef56eb94051c024e
katoken-0215 <katoken@katoken-0215.sakura.ne.jp>
2014-07-11 12:08:02 +0900

    Fix init-repo.sh; Wrong mail address

--- init-repo.sh
+++ init-repo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git config user.name "katoken-0215"
-git config user.email kenji.kato@access-company.com
+git config user.email katoken@katoken-0215.sakura.ne.jp
 
 git config credential.helper cache
