ad026b7277253fcd9be1dfb139b6bac79bd068c9
katoken-0215 <katoken@katoken-0215.sakura.ne.jp>
2015-01-10 13:40:44 +0900

    Disable js2-mode syntax checker

--- conf/progmodes/50-js2-mode.el
+++ conf/progmodes/50-js2-mode.el
@@ -33,6 +33,8 @@
              (setq tab-width 2)
              (setq js2-basic-offset 2)
              (setq-default js2-basic-offset 2)
+             (setq js2-mode-show-parse-errors nil)
+             (setq js2-mode-show-strict-warnings nil)
              (setq tab-always-indent t)
              (show-paren-mode 1)
              (whitespace-mode 1)
