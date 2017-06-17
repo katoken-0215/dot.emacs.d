a2c5b4ca4b82e706e4c1893390c6ded9538702b6
katoken-0215 <katoken@katoken-0215.sakura.ne.jp>
2017-06-17 20:56:07 +0900

    Need call package-initialize on start up

--- init.el
+++ init.el
@@ -25,6 +25,13 @@
 ;; 
 ;; For more information, please refer to [http://unlicense.org]
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 (require 'init-loader)
 (init-loader-load (expand-file-name "~/.emacs.d/conf"))
